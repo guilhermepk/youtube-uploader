efb2a8bf6630bf745ac4aa38c8f8b8563ef29349
Guilherme Prigol Kramer <112448450+guilhermepk@users.noreply.github.com>
2026-06-11 15:16:55 -0300

    secure data manager no appmodule

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { GoogleModule } from './apis/google/google.module';
 import { FileManagerModule } from './file-manager/file-manager.module';
+import { SecureDataManagerModule } from './secure-data-manager/secure-data-manager.module';
 
 @Module({
   imports: [
+    SecureDataManagerModule,
     GoogleModule,
     FileManagerModule
   ]
